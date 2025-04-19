f8cb79e196ad42513bb45e4c7c8d66e9a7d1c45f
vexedstupidity <vexedstupidity@gmail.com>
2025-04-20 03:12:33 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: ["http://localhost:3000", "http://localhost:3001", "https://vexed-task-manager.netlify.app/"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
